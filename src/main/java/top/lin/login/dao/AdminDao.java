bc9b08aa050076943a7cab09e434682fb3d61f27
Jungle Lin <linjy_eclipse@163.com>
2019-07-05 15:30:51 +0800

    更新依赖和配置

--- src/main/java/top/lin/login/dao/AdminDao.java
+++ src/main/java/top/lin/login/dao/AdminDao.java
@@ -1,10 +1,13 @@
 package top.lin.login.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import top.lin.login.entity.Admin;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Jungle Lin
  * Date: 2019/7/5
  * Time: 10:54
  */
-public interface AdminDao {
+public interface AdminDao /*extends JpaRepository<Admin, Integer>*/ {
 }
